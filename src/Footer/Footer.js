0913c803116dc273eea37da5188ac78cb63ce950
Beatrice <beatrice.opara@softwire.com>
2020-02-26 12:49:11 +0000

    Header and Footer
    added detail to header and footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,7 +4,8 @@ import './Footer.scss';
 function Footer() {
     return (
         <footer>
-            
+            <div className="container">&copy; 2020 - MyFace<a>Privacy</a>
+            </div>
         </footer>
     );
 
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,7 +5,22 @@ function Header() {
     return (
         <header>
             <nav>
-                
+                <a>MyFace</a>
+                <ul>
+                    
+                    <li>
+                        <a>Posts</a>
+                    </li>
+                    <li>
+                        <a>Users</a>
+                    </li>
+                    <li>
+                        <a>Privacy</a>
+                    </li>
+                    <li>
+                        <a>Create</a>
+                    </li>
+                </ul>
             </nav>
         </header>
     );
