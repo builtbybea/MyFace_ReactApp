0b373098d210650b19eb7118b97b638a0a4daca5
Beatrice <beatrice.opara@softwire.com>
2020-02-26 12:52:25 +0000

    anchor tag in footer
    added href to anchor tag in footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.scss';
 function Footer() {
     return (
         <footer>
-            <div className="container">&copy; 2020 - MyFace<a>Privacy</a>
+            <div className="container">&copy; 2020 - MyFace <a href={""}>Privacy</a>
             </div>
         </footer>
     );
